53eb02c359c42fee7b518cda27c92d3710844caf
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-04-08 11:30:17 +0300

    Minor refactoring related to imports.

--- app/src/main/java/com/ivanbo97/mqttclient/MainActivity.java
+++ app/src/main/java/com/ivanbo97/mqttclient/MainActivity.java
@@ -1,20 +1,19 @@
 package com.ivanbo97.mqttclient;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.github.mikephil.charting.charts.LineChart;
 import com.ivanbo97.mqttclient.actions.MqttClientActions;
 import com.ivanbo97.mqttclient.chart.SensorDataChart;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    private TextView dataReceived ;
+    private TextView dataReceived;
     private LineChart lineChart;
     private SensorDataChart dataChart;
     private static Context mainActivityContext;
@@ -33,16 +32,16 @@ public class MainActivity extends AppCompatActivity {
         dataReceived = (TextView) findViewById(R.id.dataReceived);
         lineChart = (LineChart) findViewById(R.id.chart);
         dataChart = new SensorDataChart(lineChart);
-        mqttClientActions = new MqttClientActions(lineChart,dataChart, dataReceived,this);
+        mqttClientActions = new MqttClientActions(lineChart, dataChart, dataReceived, this);
         mqttClientActions.clientConnect();
     }
 
-    public static void connectionLostToast (){
+    public static void connectionLostToast() {
         Toast toast = Toast.makeText(mainActivityContext, "Lost connection to mqtt broker...Trying to reconnect...", Toast.LENGTH_SHORT);
         toast.show();
     }
 
-    public static void connectionToBrokerFailed (){
+    public static void connectionToBrokerFailed() {
         Toast toast = Toast.makeText(mainActivityContext, "Connection to broker failed. Please check your network connection.", Toast.LENGTH_SHORT);
         toast.show();
 
