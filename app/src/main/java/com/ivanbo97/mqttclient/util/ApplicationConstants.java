5cd1e0051e19c28804defda4f2ee64ee8ca6a318
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-04-09 15:30:54 +0300

    Additional class for storing app constants.

---
+++ app/src/main/java/com/ivanbo97/mqttclient/util/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.ivanbo97.mqttclient.util;
+
+public final class ApplicationConstants {
+
+    //===API URL =====
+    public static final String PLANT_API_URL = "https://trefle.io/api/v1/plants/search?";
+    public static final String USER_TOKEN = "token=02yQU3aXVB1CIjuk52wJK5DfpN2NsmFPpa8rs7Fh2Vg";
+    public static final String QUERY_PARAM = "&q=";
+}
